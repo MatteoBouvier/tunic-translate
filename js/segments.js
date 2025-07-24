fd1ad36a879df63811bcbf6d155fc1afa8fa2a61
Matteo Bouvier <matteo.bouvier@lyon.unicancer.fr>
2025-07-25 00:04:08 +0200

    feat: basic parsing of typed character

--- js/segments.js
+++ js/segments.js
@@ -1,52 +1,52 @@
 const vowels = {
-    "00001": "é",
-    "00011": "u",
-    "00010": "è",
-    "00101": "i",
-    "00110": "e",
-    "00111": "y",
-    "01000": "á",
-    "01100": "a",
-    "01101": "ï",
-    "01111": "o",
-    "10000": "à",
-    "10111": "ÿ",
-    "11000": "â",
+    "00001": "à",
+    "00010": "á",
+    "00011": "â",
+    "00110": "a",
+    "00111": "ä",
+    "01000": "è",
+    "01100": "e",
+    "01111": "ê",
+    "10000": "é",
+    "11000": "u",
+    "10100": "i",
+    "10110": "ï",
+    "10111": "î",
     "11011": "û",
-    "11100": "ä",
-    "11101": "î",
-    "11110": "ê",
+    "11100": "y",
+    "11101": "ÿ",
+    "11110": "o",
     "11111": "ô",
 };
 const consonants = {
-    "0000011": "c",
-    "0000111": "ç",
-    "0001100": "v",
-    "0110100": "d",
-    "0111000": "f",
-    "0111001": "h",
-    "0111010": "ħ",
-    "0111100": "s",
-    "0111111": "w",
-    "1100001": "l",
-    "1100010": "j",
+    "1100000": "c",
+    "1110000": "ç",
+    "0011000": "v",
+    "0010110": "d",
+    "0001110": "f",
+    "1001110": "h",
+    "0101110": "ħ",
+    "0011110": "s",
+    "1111110": "w",
+    "1000011": "l",
+    "0100011": "j",
     "1100011": "ß",
-    "1100101": "q",
-    "1101001": "k",
-    "1101111": "z",
-    "1110000": "b",
-    "1110001": "g",
-    "1110011": "m",
-    "1110100": "µ",
-    "1110101": "r",
-    "1111000": "p",
-    "1111010": "t",
-    "1111100": "n",
+    "1010011": "q",
+    "1001011": "k",
+    "1111011": "z",
+    "0000111": "b",
+    "1000111": "g",
+    "1100111": "m",
+    "0010111": "µ",
+    "1010111": "r",
+    "0001111": "p",
+    "0101111": "t",
+    "0011111": "n",
     "1111111": "x",
 };
 
 /**
- * @param {string} code
+ * @param {string} code binary representation of active segments
  * @param {string} letter
  * @param {bool} is_vowel
  * @returns {HTMLDivElement}
@@ -63,12 +63,21 @@ function build_letter(code, letter, is_vowel) {
     let box = document.createElement("div");
     box.classList.add("segment_box", "small")
 
-    let prefix = is_vowel ? "v" : "c";
+    let prefix;
+    let length;
+    if (is_vowel) {
+        prefix = "v";
+        length = 5;
+    } else {
+        prefix = "c";
+        length = 7;
+    }
 
-    for (const [i, active] of [...code].entries()) {
-        if (active == "1") {
+    let code_value = parseInt(code, 2);
+    for (const i of Array(length).keys()) {
+        if (code_value & (2 ** i)) {
             segment = document.createElement("div");
-            segment.classList.add("segment", prefix + (i + 1));
+            segment.classList.add("segment", prefix + i);
             segment.dataset.status = "on";
             box.appendChild(segment)
         }
@@ -98,6 +107,53 @@ function setup() {
 function segment_mouseup(segment) {
     let new_status = segment.dataset.status == "off" ? "on" : "off";
     segment.dataset.status = new_status;
+}
+
+function write_character() {
+    let box = document.querySelector("#input_box").children[0];
+
+    let vowel_code = 0;
+    let consonant_code = 0;
+
+    for (const segment of box.querySelectorAll(".vowel")) {
+        if (segment.dataset.status == "on") {
+            let index = parseInt(segment.classList[2][1]);
+            vowel_code += 2 ** index;
+        }
+    }
+
+    for (const segment of box.querySelectorAll(".consonant")) {
+        if (segment.dataset.status == "on") {
+            let index = parseInt(segment.classList[2][1]);
+            consonant_code += 2 ** index;
+        }
+    }
+
+    if (vowel_code + consonant_code == 0) { return; }
+
+    let vowel = "";
+    let consonant = "";
+
+    if (vowel_code != 0) {
+        vowel = vowels[vowel_code.toString(2).padStart(5, "0")];
+    }
+    if (consonant_code != 0) {
+        consonant = consonants[consonant_code.toString(2).padStart(7, "0")];
+    }
+
+    if (vowel === undefined) {
+        console.log("invalid vowel");
+        // blink red
+    }
+
+    if (consonant === undefined) {
+        console.log("invalid consonant");
+        // blink red
+    }
+
+    if (vowel != undefined && consonant != undefined) {
+        console.log(consonant + vowel);
+    }
 }
 
 setup()
