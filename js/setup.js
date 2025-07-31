7db2f6ee3ea027db3b0e7da1bf29dd711dd98ba5
Matteo Bouvier <matteo.bouvier@lyon.unicancer.fr>
2025-08-01 01:36:45 +0200

    feat: re-done key-binding handling

--- js/setup.js
+++ js/setup.js
@@ -7,110 +7,169 @@ import { make_text_buffer, current, find_nearest, set_active } from "./text.js";
 
 let key_buffer = "";
 
+/**
+ * @readonly
+ * @enum {string}
+ */
+const Mode = Object.freeze({
+    normal: "normal",
+    insert: "insert"
+})
+
+const key_binding = {
+    [Mode.normal]: {
+        h: {
+            action: () => {
+                let sibling = find_nearest(current.active, "left");
+                if (sibling !== null) {
+                    set_active(sibling);
+                }
+            }
+        },
+        l: {
+            action: () => {
+                let sibling = find_nearest(current.active, "right");
+                if (sibling !== null) {
+                    set_active(sibling);
+                }
+            }
+        },
+        k: {
+            action: () => {
+                let sibling = find_nearest(current.active, "up");
+                if (sibling !== null) {
+                    set_active(sibling);
+                }
+            }
+        },
+        j: {
+            action: () => {
+                let sibling = find_nearest(current.active, "down");
+                if (sibling !== null) {
+                    set_active(sibling);
+                }
+            }
+        },
+        i: {
+            action: () => { current.mode = Mode.insert }
+        }
+    },
+    [Mode.insert]: {
+        Escape: {
+            action: () => { current.mode = Mode.normal }
+        }
+    }
+}
+
 function handle_keybinding(event) {
     // disable default Tab action
     if (event.keyCode === 9) {
         event.preventDefault();
     }
 
-    if (event.code == "BracketLeft") {
-        if (event.shiftKey) {
-            key_buffer = "dieresis";
-        } else {
-            key_buffer = "circumflex";
-        }
-
-        return;
+    let action = key_binding[current.mode][event.key].action;
+    if (action != undefined) {
+        action();
     }
 
-    let key = event.key;
-
-    if (key == "æ") {
-        key = "á";
-    }
-    else if (key == " ") {
-        key = "";
-        write_character(true);
-    }
-    else if (key == "Delete") {
-        key = "";
-        reset_character();
-    }
-    else if (key == "Backspace") {
-        key = "";
-        reset_character(1);
-    }
-    else if (key == "Tab") {
-        key = "";
-        add_character_if_set();
-    }
-    else if (key == "Escape") {
-        key = "";
-        current.active.classList.remove("active");
-    }
-    else if (key_buffer == "circumflex") {
-        if (key === 'a') {
-            key = "â";
-        } else if (key == 'e') {
-            key = "ê";
-        } else if (key == 'i') {
-            key = "î";
-        } else if (key == "o") {
-            key = "ô";
-        } else if (key == "u") {
-            key = "û";
-        } else {
-            key = "";
-        }
-    }
-    else if (key_buffer == "dieresis") {
-        if (key === 'a') {
-            key = "ä";
-        } else if (key == 'i') {
-            key = "ï";
-        } else if (key == "y") {
-            key = "ÿ";
-        } else {
-            key = "";
-        }
-    }
-    key_buffer = "";
-
-    if (current.mode === "normal") {
-        if (key === "h") {
-            let sibling = find_nearest(current.active, "left");
-            if (sibling !== null) {
-                set_active(sibling);
-            }
-        }
-        else if (key === "l") {
-            let sibling = find_nearest(current.active, "right");
-            if (sibling !== null) {
-                set_active(sibling);
-            }
-        }
-        else if (key === "k") {
-            let sibling = find_nearest(current.active, "up");
-            if (sibling !== null) {
-                set_active(sibling);
-            }
-        }
-        else if (key === "j") {
-            let sibling = find_nearest(current.active, "down");
-            if (sibling !== null) {
-                set_active(sibling);
-            }
-        }
-    }
-    else if (current.mode === "insert") {
-        if (key === "" || event.altKey || event.shiftKey || event.ctrlKey || event.metaKey) { return; }
-
-        if (vowels_list.indexOf(key) !== -1) {
-            set_vowel(key);
-        } else if (consonants_list.indexOf(key) !== -1) {
-            set_consonant(key);
-        }
-    }
+    // if (event.code == "BracketLeft") {
+    //     if (event.shiftKey) {
+    //         key_buffer = "dieresis";
+    //     } else {
+    //         key_buffer = "circumflex";
+    //     }
+    //
+    //     return;
+    // }
+    //
+    // let key = event.key;
+    //
+    // if (key == "æ") {
+    //     key = "á";
+    // }
+    // else if (key == " ") {
+    //     key = "";
+    //     write_character(true);
+    // }
+    // else if (key == "Delete") {
+    //     key = "";
+    //     reset_character();
+    // }
+    // else if (key == "Backspace") {
+    //     key = "";
+    //     reset_character(1);
+    // }
+    // else if (key == "Tab") {
+    //     key = "";
+    //     add_character_if_set();
+    // }
+    // else if (key == "Escape") {
+    //     key = "";
+    //     current.active.classList.remove("active");
+    // }
+    // else if (key_buffer == "circumflex") {
+    //     if (key === 'a') {
+    //         key = "â";
+    //     } else if (key == 'e') {
+    //         key = "ê";
+    //     } else if (key == 'i') {
+    //         key = "î";
+    //     } else if (key == "o") {
+    //         key = "ô";
+    //     } else if (key == "u") {
+    //         key = "û";
+    //     } else {
+    //         key = "";
+    //     }
+    // }
+    // else if (key_buffer == "dieresis") {
+    //     if (key === 'a') {
+    //         key = "ä";
+    //     } else if (key == 'i') {
+    //         key = "ï";
+    //     } else if (key == "y") {
+    //         key = "ÿ";
+    //     } else {
+    //         key = "";
+    //     }
+    // }
+    // key_buffer = "";
+    //
+    // if (current.mode === "normal") {
+    //     if (key === "h") {
+    //         let sibling = find_nearest(current.active, "left");
+    //         if (sibling !== null) {
+    //             set_active(sibling);
+    //         }
+    //     }
+    //     else if (key === "l") {
+    //         let sibling = find_nearest(current.active, "right");
+    //         if (sibling !== null) {
+    //             set_active(sibling);
+    //         }
+    //     }
+    //     else if (key === "k") {
+    //         let sibling = find_nearest(current.active, "up");
+    //         if (sibling !== null) {
+    //             set_active(sibling);
+    //         }
+    //     }
+    //     else if (key === "j") {
+    //         let sibling = find_nearest(current.active, "down");
+    //         if (sibling !== null) {
+    //             set_active(sibling);
+    //         }
+    //     }
+    // }
+    // else if (current.mode === "insert") {
+    //     if (key === "" || event.altKey || event.shiftKey || event.ctrlKey || event.metaKey) { return; }
+    //
+    //     if (vowels_list.indexOf(key) !== -1) {
+    //         set_vowel(key);
+    //     } else if (consonants_list.indexOf(key) !== -1) {
+    //         set_consonant(key);
+    //     }
+    // }
 
 }
 
